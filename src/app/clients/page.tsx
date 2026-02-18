fc509ccba9c4dba7547a33aea28d424c11cbbed1
RoberGF <rgfrasta@gmail.com>
2026-02-18 11:01:01 +0100

    mini refactor

--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -2,15 +2,17 @@ export default async function ClientsPage() {
   const clientsList = await fetch("http://localhost:3000/api/v1/clients");
   const result = await clientsList.json();
 
+  type Client = { id: number; name: string; email: string };
+
   return (
     <>
       <h1 className="text-secondary">Client list</h1>
       <div>
         <ul>
-          {result.map((client: { id: number; name: string; email: string }) => (
-            <li key={client.id} className="container flex gap-8">
-              <h3 className="text-accent">{client.name}</h3>
-              <h3 className="text-muted">{client.email}</h3>
+          {result.map(({ id, name, email }: Client) => (
+            <li key={id} className="container flex gap-8">
+              <h3 className="text-accent">{name}</h3>
+              <h3 className="text-muted">{email}</h3>
             </li>
           ))}
         </ul>

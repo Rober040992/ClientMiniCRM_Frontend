e261b8cc543fe7a19bfb95ce522b3da00ecee0b9
RoberGF <rgfrasta@gmail.com>
2026-02-20 11:12:04 +0100

    baseURL to env

--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -1,7 +1,8 @@
 import { Card } from "@/components/ui/card";
 
 export default async function ClientsPage() {
-  const clientsList = await fetch("http://localhost:3000/api/v1/clients");
+  const clientsFetch = `${process.env.API_BASE_URL}/clients`
+  const clientsList = await fetch(clientsFetch);
   const result = await clientsList.json();
 
   type Client = { id: number; name: string; email: string };
@@ -13,8 +14,9 @@ export default async function ClientsPage() {
         <ul className="container flex flex-col gap-4">
           {result.map(({ id, name, email }: Client) => (
             <Card key={id} className="flex gap-8">
-              <h4 className="">Name: {name}</h4>
-              <h4 className="">Email: {email}</h4>
+              <h4 className="ml-4">Name: {name}</h4>
+              <h4 className="ml-4">Email: {email}</h4>
+              <h4 className="ml-4">Id: {id}</h4>
             </Card>
           ))}
         </ul>
